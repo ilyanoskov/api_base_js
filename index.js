691935d158d3565978f4b0b86facb514d500a85a
Ilya Noskov <ilyaanoskov@gmail.com>
2017-06-23 12:13:18 +0300

    adds preliminary swagger support, fixes issues with rootRequire

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 'use strict';
-global.rootRequire = function(name) {
+
+global.libRequire = function(name) {
   return require(__dirname + '/' + name);
 };
 
 module.exports = (app) => {
     const Routes = require('./lib/routes/helpers')(app);
-    let _ = require('lodash');
-    let Model = require('./lib/models/models.js');
-    let generateEndpoints = require('./lib/routes/index.js').generateEndpoints;
+    const _ = require('lodash');
+    const Model = require('./lib/models/models.js');
+    const generateEndpoints = require('./lib/routes/index.js').generateEndpoints;
+    const swaggerTools = require('swagger-tools');
+    const generateSwaggerfile = require('./lib/swagger/index.js').generateSwaggerfile;
+
+    //generate swaggerDoc
+    const swaggerDoc = generateSwaggerfile();
+    console.log(swaggerDoc);
+    
+    //initialize swaggerUI and generate documentation
+    /*
+    swaggerTools.initializeMiddleware(swaggerDoc, (middleware) => {
+        app.use(middleware.swaggerUi());
+    });
+    */
+
 
     const defineEndpoints = (endpoints) => {
         endpoints.forEach((endpoint) => {
@@ -18,9 +33,6 @@ module.exports = (app) => {
         app.get('/v0.1/public-endpoints', generateEndpoints(endpoints)); 
     };
 
-    const generateDocumentation = (endpoints) => {
-           
-    };
 
     return {
         defineEndpoints : defineEndpoints,
